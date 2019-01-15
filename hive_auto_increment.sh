6353f2a10fda47b6e1dfee384569e15737dab750
Harkamal Dadwal <dadwal.harkamal@gmail.com>
2019-01-15 15:32:11 -0700

    Create hive_auto_increment.sh

---
+++ hive_auto_increment.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+#### This script basically implements the auto increment functinality for HIVE tables ###
+### Create atemporrary table in the DB with the max value from the table for which you want to implemen the autoincrement feature###
+### This can be achieved by using simple insert using selelct stmt###
+#### aftre that use the below hive stmts, use this alongwith the actual insert g=hive stmts. #########
+
+var=$(hive -e "use test; select * from  t_auto_incr")
+echo $var
+hive -e "use test; select row_sequence()+$var"
